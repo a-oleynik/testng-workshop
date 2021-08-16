4e228f49c5a1302d57f78a60590a1109feae512b
Andrey Oleynik <andrei.oleynik@gmail.com>
2021-08-17 00:11:51 +0200

    Switching to TestNG 7.3.0. Updating dataprovider

---
+++ src/main/java/com/dataart/qa/workshop/testng/annotations/DataSource.java
@@ -0,0 +1,9 @@
+package com.dataart.qa.workshop.testng.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DataSource {
+    String path();
+}
